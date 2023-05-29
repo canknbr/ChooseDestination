1557e8675fc842d48a67b37a5277030443fc861a
canknbr <cankanbur1@gmail.com>
2023-05-29 14:32:31 +0300

    Initial Commit

---
+++ ChooseDestination/ChooseDestinationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChooseDestinationApp.swift
+//  ChooseDestination
+//
+//  Created by Can Kanbur on 29.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChooseDestinationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
